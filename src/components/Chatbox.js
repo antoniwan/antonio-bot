60e84b25844c2e9eb360e5d00dcf6e5e400c85db
Antonio Rodríguez <antoniwan@gmail.com>
2017-06-03 11:44:23 -0400

    Initialize Chatinput and Chatbox components and bsae styles

--- src/components/Chatbox.js
+++ src/components/Chatbox.js
@@ -1,11 +1,35 @@
 import React from 'react';
 
+import './Chatbox.css';
+
+class Chatinput extends React.Component {
+  constructor (props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit (event) {
+    event.preventDefault();
+    console.log('chatinput submitted');
+  }
+  render () {
+    return (
+      <form className="chatinput" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Write me something..." />
+        <button>
+          Send
+        </button>
+      </form>
+    )
+  }
+}
+
 class Chatbox extends React.Component {
   render () {
     return (
       <div>
-        <div className="chatbox">
-
+        <Chatinput />
+        <div className='chatbox'>
+          hola
         </div>
       </div>
     )
