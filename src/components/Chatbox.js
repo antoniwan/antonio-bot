e14ddaeb98d3d3be62805b2b8c3fe588f3a15771
Antonio Rodríguez <antoniwan@gmail.com>
2017-06-03 11:16:59 -0400

    Initialize the Chatbox component with a stub render method

---
+++ src/components/Chatbox.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Chatbox extends React.Component {
+  render () {
+    return (
+      <div>
+        <div className="chatbox">
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Chatbox;
--- src/layouts/App.js
+++ src/layouts/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+
 import './App.css';
+import Chatbox from '../components/Chatbox';
 
 class App extends Component {
+
   render() {
     return (
-      <div>
-        <h1>Hello create-react-app</h1>
+      <div className='container'>
+        <div className="full_width_column">
+          <h1>Meet, Antonio bot!</h1>
+          <Chatbox />
+        </div>
       </div>
     );
   }
