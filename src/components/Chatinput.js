d9953a38d853b3f78be7adcd97fd65cc429fd519
Antonio Rodriguez <antoniwan@gmail.com>
2018-01-07 14:07:07 -0500

    prettier

--- src/components/Chatinput.js
+++ src/components/Chatinput.js
@@ -1,42 +1,47 @@
-import React from 'react';
-import './Chatinput.css';
+import React from "react";
+import "./Chatinput.css";
 
 class Chatinput extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
 
     this.state = {
-      message: '',
-    }
+      message: ""
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange (event) {
+  handleChange(event) {
     const newState = {
-      'message': event.target.value
+      message: event.target.value
     };
     this.setState(newState);
     this.props.callbackParent(newState);
   }
 
-  handleSubmit (event) {
+  handleSubmit(event) {
     event.preventDefault();
     this.props.onSubmit(event);
     this.setState({
-      message: ''
+      message: ""
     });
   }
 
-  render () {
+  render() {
     return (
       <form className="chatinput" onSubmit={this.handleSubmit}>
-        <input type="text" value={this.state.message} placeholder="Write me something..." onChange={this.handleChange} />
+        <input
+          type="text"
+          value={this.state.message}
+          placeholder="Write me something..."
+          onChange={this.handleChange}
+        />
         <button type="submit" disabled={!this.state.message}>
           Send
         </button>
       </form>
-    )
+    );
   }
 }
 
