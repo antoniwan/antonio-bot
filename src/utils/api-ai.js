d77fdaf2e58b786c50eddab4cafe59239a4725f5
Antonio Rodriguez <antoniwan@gmail.com>
2018-01-07 12:57:06 -0500

    I have to include this third-party package manually.
    Because they don’t compile their code to ES5 before publishing it to npm.

--- src/utils/api-ai.js
+++ src/utils/api-ai.js
@@ -1,22 +1,23 @@
-import { ApiAiClient } from "api-ai-javascript";
+import { ApiAiClient } from "./api-ai-client/ApiAiClient";
 
 const apiAiAccessToken = process.env.REACT_APP_API_AI_ACCESS_TOKEN;
 const client = new ApiAiClient({
-  accessToken: apiAiAccessToken,
+  accessToken: apiAiAccessToken
 });
 
 class ApiAi {
-  constructor () {
+  constructor() {
     // Stub constructor
-    console.log('api ai class initialized and constructed!');
+    console.log("api ai class initialized and constructed!");
   }
-  test () {
+  test() {
     // Stub public prototype test method
-    return client.textRequest('Hola!')
-      .then((response) => {
+    return client
+      .textRequest("Hola!")
+      .then(response => {
         console.log(response);
       })
-      .catch((error) => {
+      .catch(error => {
         console.log(error);
       });
   }
@@ -27,12 +28,13 @@ class ApiAi {
 
 export default ApiAi;
 
-export const sendMessage = (text) => {
-  return client.textRequest(text)
-      .then((response) => {
-        return response;
-      })
-      .catch((error) => {
-        return error;
-      });
+export const sendMessage = text => {
+  return client
+    .textRequest(text)
+    .then(response => {
+      return response;
+    })
+    .catch(error => {
+      return error;
+    });
 };
